59e20be9adb4346b22f7544f69d1ba27368dc948
John Patrick Amata <johnamata97@gmail.com>
2020-05-16 02:46:09 +0800

    first

---
+++ cldprogrsy/Models/Reservations.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+namespace cldprogrsy.Models
+{
+    public class Reservations
+    {
+        public int ReservationId { get; set; }
+        public int NumberOfPeople { get; set; }
+        public string Day { get; set; }
+        public string Time { get; set; }
+    }
+}
---
+++ cldprogrsy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cldprogrsy.Startup))]
+namespace cldprogrsy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
